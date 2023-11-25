4a731cd7506b4b9c7b0d962457ad65d51be226f9
GustavooBueno <gustavo.ibis.gb@gmail.com>
2023-11-25 11:56:27 -0300

    Adicionando CORS

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const cors = require('cors');
 
 require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 5000
+
+//Permitir solicitações de todas as origens
+app.use(cors());
 
 //Parsing middleware
 app.use(bodyParser.urlencoded({extended: false}))
